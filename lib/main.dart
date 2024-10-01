f35a4f82a4534404a1463695313e79a3e38575e0
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-10-01 18:08:50 -0500

    A barebone concept for the desktop environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,73 @@
-import 'dart:ui';
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runWidget(MyApp());
+  await SystemChannels.platform.invokeMethod(
+    "add_view",
+    [
+      {
+        "name": "aaaa",
+        "width": 400,
+        "height": 1080,
+        "exclusive_zone": 400,
+        "layer": 1,
+        "anchors": {
+          "top": false,
+          "left": true,
+          "bottom": false,
+          "right": false,
+        },
+        "keyboard_interactivity": 0,
+        "margin": [0, 0, 0, 0],
+      }
+    ],
+  );
+
+  runWidget(
+    Column(
+      children: [
+        View(
+          view: PlatformDispatcher.instance.view(id: 0)!,
+          child: MyApp(),
+        ),
+        View(
+          view: PlatformDispatcher.instance.view(id: 1)!,
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Container(
+              width: 400,
+              height: 1080,
+              color: Colors.blue,
+              child: Column(
+                children: [
+                  FilledButton(
+                    onPressed: () async {},
+                    child: Text(
+                      "hello",
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () async {},
+                    child: Text(
+                      "hello",
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () async {},
+                    child: Text(
+                      "hello",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -17,42 +79,19 @@ class _MyAppState extends State<MyApp> {
   bool _menu = false;
   @override
   Widget build(BuildContext context) {
-    final ever = Whatever(id: 1).create();
-    return PlatformViewLink(
-      viewType: "view-type",
-      surfaceFactory: (_, __) {
-        print("Are We here");
-        return Container(
-          width: 600,
-          height: 600,
-          color: Colors.red,
-        );
-      },
-      onCreatePlatformView: (_) {
-        return Whatever(id: 1);
-      },
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        width: 600,
+        height: 100,
+        color: Colors.red,
+        child: FilledButton(
+          onPressed: () async {},
+          child: Text(
+            "hello",
+          ),
+        ),
+      ),
     );
-  }
-}
-
-class Whatever extends PlatformViewController {
-  final int id;
-  Whatever({required this.id}) : super();
-
-  @override
-  Future<void> clearFocus() async {}
-
-  @override
-  Future<void> dispatchPointerEvent(PointerEvent event) async {}
-
-  @override
-  Future<void> dispose() async {}
-
-  @override
-  int get viewId => this.id;
-@override
-  Future<void> create({Size? size, Offset? position}) {
-    // TODO: implement create
-    return super.create(size: size, position: position);
   }
 }
