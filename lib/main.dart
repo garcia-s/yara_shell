c4680baf09293b8f8162d593f269ddf78c867fcf
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-10-01 21:41:25 -0500

    Started the Ybar, pronounced WhyBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,69 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SystemChannels.platform.invokeMethod(
-    "add_view",
-    [
-      {
-        "name": "aaaa",
-        "width": 400,
-        "height": 1080,
-        "exclusive_zone": 400,
-        "layer": 1,
-        "anchors": {
-          "top": false,
-          "left": true,
-          "bottom": false,
-          "right": false,
-        },
-        "keyboard_interactivity": 0,
-        "margin": [0, 0, 0, 0],
-      }
-    ],
-  );
 
   runWidget(
     Column(
       children: [
         View(
           view: PlatformDispatcher.instance.view(id: 0)!,
-          child: MyApp(),
-        ),
-        View(
-          view: PlatformDispatcher.instance.view(id: 1)!,
-          child: Directionality(
-            textDirection: TextDirection.ltr,
-            child: Container(
-              width: 400,
-              height: 1080,
-              color: Colors.blue,
-              child: Column(
-                children: [
-                  FilledButton(
-                    onPressed: () async {},
-                    child: Text(
-                      "hello",
-                    ),
-                  ),
-                  FilledButton(
-                    onPressed: () async {},
-                    child: Text(
-                      "hello",
-                    ),
-                  ),
-                  FilledButton(
-                    onPressed: () async {},
-                    child: Text(
-                      "hello",
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          child: YBar(),
         ),
       ],
     ),
   );
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _menu = false;
+class YBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final view = View.of(context);
     return Directionality(
       textDirection: TextDirection.ltr,
-      child: Container(
-        width: 600,
-        height: 100,
-        color: Colors.red,
-        child: FilledButton(
-          onPressed: () async {},
-          child: Text(
-            "hello",
-          ),
+      child: MediaQuery(
+        data: MediaQueryData.fromView(view),
+        child: Row(
+          children: [
+            FilledButton(
+              onPressed: () async {},
+              child: Icon(
+                Icons.power_settings_new,
+              ),
+            ),
+            FilledButton(
+              onPressed: () async {},
+              child: Icon(
+                Icons.power_settings_new,
+              ),
+            ),
+            FilledButton(
+              onPressed: () async {},
+              child: Icon(
+                Icons.power_settings_new,
+              ),
+            ),
+            Expanded(
+              child: Offstage(),
+            ),
+            FilledButton(
+              onPressed: () async {},
+              child: Icon(
+                Icons.power_settings_new,
+              ),
+            ),
+
+            FilledButton(
+              onPressed: () async {},
+              child: Icon(
+                Icons.power_settings_new,
+              ),
+            ),
+          ],
         ),
       ),
     );
